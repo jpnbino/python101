793ef70cdef98ba8a54e33ddf6e3a2aabc71ef30
Joao Paulo Bino <jpnogg@gmail.com>
2022-08-05 10:38:33 +0200

    added source file

---
+++ weather.py
@@ -0,0 +1,19 @@
+from urllib import response
+from platform import python_version
+import requests
+
+print("--------------------------------------")
+print("Current Python Version-", python_version())
+print("--------------------------------------")
+
+
+API_KEY = {"81bc5b7c36d8cca6868ca53d89d65122"}
+BASE_URL = {"https://api.openweathermap.org/data/2.5/weather"}
+city = input("Enter the city: ")
+request_url = f"{BASE_URL}?&appid={API_KEY}&q={city}"
+response = requests.get(request_url)
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print("Oops!")
