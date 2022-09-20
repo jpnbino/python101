5443de2ceb0a90ff8ea286c16c48bd2d1f50155c
Joao Paulo Bino <jpnogg@gmail.com>
2022-09-20 11:18:25 +0200

    counting line of code

--- regex/read_fil.py
+++ regex/read_fil.py
@@ -1,27 +1,49 @@
 import enum
-from tokenize import blank_re
+import re
 
 comment_cnt = 0
 loc_cnt = 0 
 mline_cnt = 0
 blankline_cnt = 0
+is_multiline = 0
 
-
-with open( "mem_map.h",'r') as file:
+with open( "tsl2561_lux_calculus.c",'r') as file:
     for l_no, line in enumerate(file):
         
         #search multiline comment
         if "/*" in line:
-            mline_cnt = l_no
+
+            line = line.strip()
+            match = (re.search("/\*", line))
+            print(l_no, match)
+            
+            if ( match.start() != 0):
+                loc_cnt += 1
+
+            if "*/" in line:
+                comment_cnt += 1    
+            else:
+                mline_cnt = l_no
+                is_multiline = 1
             continue
 
         if "*/" in line:
             mline_cnt = l_no - mline_cnt + 1
             comment_cnt += mline_cnt
+            is_multiline = 0
+            continue
+
+        if is_multiline:
             continue
 
         if "//" in line:
             comment_cnt += 1
+            line = line.strip()
+            match = (re.search("//", line))
+            
+            if ( match.start() != 0):
+                loc_cnt += 1
+                
             continue
         
         #check empty line
